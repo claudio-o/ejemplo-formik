fe660d88c7a9835436be2e76e33347f108399d50
claudio-o <36342826+claudio-o@users.noreply.github.com>
2022-01-28 07:40:14 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+//import App from "./App";
+//import Verification from "./Verification";
+import Captura from "./Captura";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Captura />, rootElement);
